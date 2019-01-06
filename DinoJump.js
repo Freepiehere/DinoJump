babdd57538408f97a9032af370e27db046039f1b
Freepiehere <40921464+Freepiehere@users.noreply.github.com>
2019-01-06 02:03:32 -0500

    Add files via upload

--- DinoJump.js
+++ DinoJump.js
@@ -140,10 +140,8 @@ function drawObstacles() {
     for (i=0;i<array.length;i++)    {
         var obstacle = array[i];
         var ctx = board.getContext("2d");
-        ctx.beginPath();
         ctx.fillStyle = "#FF0000";
         ctx.fillRect(board.width-obstacle[0],board.height-obstacle[2],obstacle[1],obstacle[2]);
-        ctx.stroke();
         array[i][0]+=speed;
     }
     if(board.width-array[0][0]<=-array[0][1])    {
